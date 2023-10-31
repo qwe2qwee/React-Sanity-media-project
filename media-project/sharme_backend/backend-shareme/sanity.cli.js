395f1a159612d2ea439f3873ec22c133aab9f3fd
Hossin <94375287+qwe2qwee@users.noreply.github.com>
2023-10-31 17:45:03 +0300

    work on the backend

---
+++ media-project/sharme_backend/backend-shareme/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'cq9p3224',
+    dataset: 'production'
+  }
+})
---
+++ media-project/sharme_backend/sanity.config.js
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'backend-shareme',
+
+  projectId: 'cq9p3224',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
