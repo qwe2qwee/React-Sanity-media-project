075584281377c4d9c58c19d885b0d834a1a107b1
Hossin <94375287+qwe2qwee@users.noreply.github.com>
2023-11-01 17:56:57 +0300

    working in frontend and some errors

--- media-project/sharme_backend/backend-shareme/schemas/index.js
+++ media-project/sharme_backend/backend-shareme/schemas/index.js
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import user from "./user"
+import comment from './comment';
+import postedBy from './postedBy';
+import save from './save';
+import pin from './pin';
+
+export const schemaTypes = [user, comment,postedBy,pin,save ]
---
+++ media-project/sharme_frontend/src/App.js
@@ -0,0 +1,14 @@
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Home from "./container/Home";
+import Login from "./components/Login";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='login' element={<Login />} />
+      <Route path='/*' element={<Home />} />
+    </Routes>
+  );
+};
+
+export default App;
